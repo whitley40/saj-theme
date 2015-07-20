49a0fd3a82998affa4cce4bffc8d7604a481e8cd
whitley40 <whitley40@hotmail.com>
2015-07-20 23:33:29 +0100

    new blank templates with varying columns added

---
+++ page-blank-1col.php
@@ -0,0 +1,42 @@
+<?php
+/*
+	Template Name: Blank One Column
+*/
+?>
+<?php get_header(); ?>
+
+		<?php
+			$args = array(
+				'post_type' => 'page'
+				);
+			$query = new WP_Query( $args );
+		?>
+
+<?php if( have_posts() ): while( have_posts() ) : the_post(); ?>
+
+<!-- if there is a featured image then stick it in here -->
+		<?php if( get_the_post_thumbnail() ) : ?>
+			<section class="saj-article-header large-12">
+				<?php the_post_thumbnail('large'); ?>
+			</section>
+		<?php endif; ?>
+<!-- //end header-->		
+
+<section class="blank-page main-content large-12">
+	<div class="centered group">
+
+	<header class="large-12 general-columns">
+		 <h1><?php the_field('blank-title'); ?></h1>
+	</header>
+
+	<section class="general-columns large-12 medium-12">
+		<?php the_field('blank-content'); ?>
+	</section>
+
+	</div>
+</section>
+
+<?php endwhile;  endif; ?>
+
+
+<?php get_footer(); ?>
