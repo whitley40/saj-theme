f544e31af906ed29d19aca9a82371c40f6832b31
whitley40 <whitley40@hotmail.com>
2015-07-22 21:49:34 +0100

    minor change

--- header.php
+++ header.php
@@ -13,7 +13,6 @@
 	<meta name="Copyright" content="" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php wp_title(); ?></title>
-
 	<?php wp_head(); ?>
 
 </head>
